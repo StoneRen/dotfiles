a65b0c1e7970d8e84df4e096119a9d1cd8c5b244
StoneRen <stoneren@outlook.com>
2023-11-20 12:54:14 +0800

    refactor(autocmds): 去掉无用代码

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,10 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local function augroup(name)
-  return vim.api.nvim_create_augroup("stonerenvim_" .. name, { clear = true })
-end
-
 vim.api.nvim_create_autocmd("TabNew", {
   once = true,
   callback = function(args)
