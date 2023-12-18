d1323d09d7ada5c43f25937cb2259c34b5db291c
StoneRen <stoneren@outlook.com>
2023-12-18 11:18:13 +0800

    feat(plugins): +eslint +rope(python的格式化)

---
+++ nvim/lua/plugins/rope.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "python-rope/ropevim",
+    ft = "python",
+    keys = {
+      { "<M-/>", "<cmd>call RopeCodeAssist()<cr>", desc = "Python Code Assist" },
+      { "<M-?>", "<cmd>call RopeLuckyAssist()<cr>", desc = "Python Lucky Assist" },
+      { "<M-c>i", "<cmd>call RopeAutoImport()<cr>", desc = "Python Auto Import" },
+      { "<M-c>g", "<cmd>call RopeGotoDefinition()<cr>", desc = "Python Goto Definition" },
+      { "<M-c>d", "<cmd>call RopeShowDoc()<cr>", desc = "Python Show Doc" },
+      { "<M-c>f", "<cmd>call RopeFindOccurrences()<cr>", desc = "Python Find Occurrences" },
+    },
+    config = function()
+      vim.g.ropevim_autoimport_modules = { "os", "torch" }
+    end,
+  },
+}
