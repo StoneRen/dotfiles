428471d9ad86f0a089d7b85a9bfdc8e3b9a229cc
StoneRen <stoneren@outlook.com>
2024-04-14 21:45:51 +0800

    feat(nvim): lsp formatting linting

--- nvim/lua/stoneren/lazy.lua
+++ nvim/lua/stoneren/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("stoneren.plugins",{
+require("lazy").setup({{import = "stoneren.plugins"},{import = "stoneren.plugins.lsp"}},{
   checker = {
     enabled = true,
     notify = false,
